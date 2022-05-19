ab19526b63094de68cf11d5130be0528e4d34c4b
Chloe-O <chloeosbourne@yahoo.co.uk>
2022-05-19 14:54:14 +0100

    delete item function added

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 
+const todoList = document.getElementById("todo-list")
 const addItemBtn = document.getElementById("add-item")
+const deleteItemBtn = document.getElementById("delete-item")
 
+addItemBtn.addEventListener("click", addItem)
 
-addItemBtn.addEventListener("click", function() {
+function addItem() {
     let todoInput = document.getElementById("todo-input").value
-    const inputStr = `<li class="todo-item">${todoInput}</li>`
-    console.log(inputStr)
-})
+        if ( todoInput === "") {
+            alert("No input")
+        } else {
+            const inputStr = `<li class="todo-item">${todoInput} <i class="fa-solid fa-xmark delete"></i></li>`
+            todoList.innerHTML += inputStr
+            deleteItem()
+        }
+    document.getElementById("todo-input").value = "";
+}
 
+
+function deleteItem() {
+    const deleteClass = document.getElementsByClassName("delete")
+
+    for (let i = 0; i < deleteClass.length; i++) {
+        deleteClass[i].onclick = function() {
+            d = this.parentElement;
+            d.style.display = "none";
+        }
+    }
+}
+
+deleteItem()
