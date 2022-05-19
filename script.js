d7fe4d52f273f1f7333b61daa31c08675b8b0536
Chloe-O <chloeosbourne@yahoo.co.uk>
2022-05-19 17:03:52 +0100

    made responsive

--- script.js
+++ script.js
@@ -3,30 +3,39 @@ const todoList = document.getElementById("todo-list")
 const addItemBtn = document.getElementById("add-item")
 const deleteItemBtn = document.getElementById("delete-item")
 
-addItemBtn.addEventListener("click", addItem)
+addItemBtn.addEventListener("click", createItem)
 
-function addItem() {
+function createItem() {
     let todoInput = document.getElementById("todo-input").value
-        if ( todoInput === "") {
-            alert("No input")
-        } else {
-            const inputStr = `<li class="todo-item">${todoInput} <i class="fa-solid fa-xmark delete"></i></li>`
-            todoList.innerHTML += inputStr
-            deleteItem()
-        }
+    if (todoInput === "") {
+        alert("No input")
+    } else {
+        const inputStr = `<li class="todo-item">${todoInput} <i class="fa-solid fa-xmark delete"></i></li>`
+        todoList.innerHTML += inputStr
+        checkItem()
+        deleteItem()
+    }
     document.getElementById("todo-input").value = "";
 }
-
 
 function deleteItem() {
     const deleteClass = document.getElementsByClassName("delete")
 
     for (let i = 0; i < deleteClass.length; i++) {
-        deleteClass[i].onclick = function() {
-            d = this.parentElement;
-            d.style.display = "none";
-        }
+        deleteClass[i].addEventListener("click", function () {
+            deleteClass[i].parentElement.style.display = "none";
+        })
     }
 }
 
+function checkItem() {
+    const todoItem = document.getElementsByClassName("todo-item")
+    for (let x = 0; x < todoItem.length; x++) {
+        todoItem[x].addEventListener("click", function () {
+            todoItem[x].classList.toggle("checked")
+        })
+    }
+}
+
+checkItem()
 deleteItem()
